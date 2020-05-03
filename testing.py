48ebf037bf1a33903a63c4c324fcba6599de7281
shlokmehrotra <shlokmehrotra@gmail.com>
2020-05-02 23:47:18 -0700

    extra file

--- testing.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = [0.0, 0.282, 0.482, 1.015, 1.515]
-y = [0.383, 2.57, 5.51, 9.98, 14.9]
-
-
-m, b = np.polyfit(np.array(x), np.array(y), 1)
-print(m, b)
