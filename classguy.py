92aeb595e1e431db037c3c2daaa7f58f7bb8bcf5
Shlok Mehrotra <shlokmehrotra@gmail.com>
2020-05-03 12:29:37 -0700

    flask start

---
+++ classguy.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import random
+import time
+from scipy.optimize import minimize
+from scipy import integrate
+import pylab as pl
+import numpy as np
+from census import Census
+import matplotlib.pyplot as plt
+from us import states
+from scipy import integrate, optimize
+import csv
+
+class Person:
+  def __init__():
+  	print("ok so function called guy")
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+  def
